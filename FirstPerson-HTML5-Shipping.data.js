6fa209c3221660d7bb1c725ad32b081ea1c7ec23
misha850 <misha850@gmail.com>
2023-05-21 11:56:59 +0700

    Update FirstPerson-HTML5-Shipping.data.js

--- FirstPerson-HTML5-Shipping.data.js
+++ FirstPerson-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Users/mike/Documents/Unreal Projects/FirstPerson/Binaries/HTML5/FirstPerson-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'FirstPerson-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/wgsg1ze5f0s4mlo/FirstPerson-HTML5-Shipping.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/wgsg1ze5f0s4mlo/FirstPerson-HTML5-Shipping.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
