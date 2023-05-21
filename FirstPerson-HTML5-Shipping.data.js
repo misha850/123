b32b20ec65e228669d36c26bdbeaacdb263d5fbd
misha850 <misha850@gmail.com>
2023-05-21 20:54:37 +0700

    Update FirstPerson-HTML5-Shipping.data.js

--- FirstPerson-HTML5-Shipping.data.js
+++ FirstPerson-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/zwlwqw0cjxcuolz/FirstPerson-HTML5-Shipping.data?dl=0';
-    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/zwlwqw0cjxcuolz/FirstPerson-HTML5-Shipping.data?dl=0';
+    var PACKAGE_NAME = 'https://aimautov.ru/app/FirstPerson-HTML5-Shipping.data';
+    var REMOTE_PACKAGE_BASE = 'https://aimautov.ru/app/FirstPerson-HTML5-Shipping.data';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
